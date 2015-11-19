d323759d9747ef959ac2abd0a18a103853103f87
ajahannes <aguerojahannes@gmail.com>
2015-11-18 18:14:16 -0800

    first commit

---
+++ www/js/controllers/GlobalController.js
@@ -0,0 +1,69 @@
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('GlobalController', GlobalController);
+
+	function GlobalController(GlobalFactory, $state, $stateParams, $scope, $window) {
+		var glob = this;
+		glob.user = {};
+//--------------- ADD THIS TO CHECK -----------------
+		glob.isLogin = true; //switch between the login and register view on the login_register.html page
+    		glob.user = {};
+   		glob.status = GlobalFactory.status;
+
+		// On Load Scroll Window To Top
+		window.scrollTo(0, 0);
+
+// --------- SIGN UP --------------------------
+		glob.signUp = function() {
+			GlobalFactory.signUp(glob.user).then(function(){
+				$state.go("Tab.Dash");
+			});
+		};
+
+		glob.signUpEs = function() {
+			GlobalFactory.signUp(glob.user).then(function(){
+				$state.go("Tab.Dash");
+			});
+		};
+
+// ---------------- SIGN IN ----------------------------------------
+		glob.signIn = function() {
+			GlobalFactory.signIn(glob.user).then(function(){
+				$state.go('Tab.Dash', {id: glob.status._id});
+			});
+		};
+		glob.signInEs = function() {
+			GlobalFactory.signIn(glob.user).then(function(){
+				$state.go('Tab.Dash', {id: glob.user._id});
+			});
+		};
+
+		// FORGOT PASSWORD? SEND EMAIL TO UPDATE
+		glob.forgot = function() {
+			GlobalFactory.forgot(glob.user).then(function() {
+				$state.go('SignIn') ;
+			}) ;
+		};
+
+//-----------UPDATE PASSWORD (LIKE EDIT)------------------
+		glob.resetPassword = function(){
+			glob.user.id = $stateParams.id ;
+			GlobalFactory.resetPassword(glob.user).then(function(res){
+				$state.go('Home');
+			});
+		};
+
+
+// LOG OUT
+		glob.logout = function(){
+			GlobalFactory.logout();
+			$state.go("Home");
+			$window.location.href=("/");
+		};
+
+		// Bring State To Document
+		$scope.$state = $state;
+
+}
+})();
